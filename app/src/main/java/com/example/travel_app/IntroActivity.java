7e3c9ed8ecee9db09c7f709a0f0da7e3dee16490
Prathmesh2011 <prathmeshrewatkar2011@gmail.com>
2024-11-09 23:51:37 +0530

    Add Firebase to Project

---
+++ app/src/main/java/com/example/travel_app/IntroActivity.java
@@ -0,0 +1,32 @@
+package com.example.travel_app;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.example.travel_app.databinding.ActivityIntroBinding;
+
+public class IntroActivity extends BaseActivity {
+
+    ActivityIntroBinding binding;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityIntroBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        binding.introBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(IntroActivity.this, MainActivity.class));
+            }
+        });
+    }
+}
